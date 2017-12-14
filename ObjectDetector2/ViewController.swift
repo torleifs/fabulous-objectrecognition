c2e3b1214e78704685641c2cbe67cd130d70573e
Torleif Sandnes <torleif.sandnes@telenordigital.com>
2017-12-14 10:08:20 +0100

    Initial Commit

---
+++ ObjectDetector2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ObjectDetector2
+//
+//  Created by Torleif Sandnes on 14/12/2017.
+//  Copyright © 2017 Telenor Capture AS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
